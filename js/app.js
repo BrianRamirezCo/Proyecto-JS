63cb0fed8756ac5e5b1c803d4f8304db839e25b6
Ramirez Brian <98188673+BrianRamirezCo@users.noreply.github.com>
2022-06-15 23:41:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,26 +19,3 @@ listaDeUsuarios.push(dataUsuarios);
 console.log(listaDeUsuarios);
 
 
-
-
-// const data = [
-//     { value: 1, name: 'Uno' },
-//     { value: 2, name: 'Dos' },
-//     { value: 3, name: 'Tres' },
-//     { value: 4, name: 'Cuatro' }
-//   ]
-  
-//   let nuevoArreglo = [];
-//   let dataObject = new Object();
-  
-//   for(i = 0; i < data.length; i++){
-//     dataObject = {};
-//     dataObject['value'] = data[i].value;
-//     dataObject['name'] = data[i].name;
-//     nuevoArreglo.push(dataObject);
-//   }
-//   console.log(nuevoArreglo);
-
-
-// // console.log(listaDeUsuarios);
-
